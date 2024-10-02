cfe75982a1cd3f0df1c3192c8f6da3d5a59380a0
soraya <108007333+sorayasuellen@users.noreply.github.com>
2024-10-02 15:08:22 -0300

    c

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-import { aleatorio, none} from './aleatorio.js';
-import { perguntas} from './perguntas.js';
+import { aleatorio, nome } from './aleatorio.js';
+import { perguntas } from './perguntas.js';
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultados = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
-const botaoIniciar = document.querySelector(".Iniciar-Btn");
+const botaoIniciar = document.querySelector(".iniciar-btn");
 const telaInicial = document.querySelector(".tela-inicial");
 
 let atual = 0;
@@ -16,39 +16,39 @@ let historiaFinal = "";
 
 botaoIniciar.addEventListener('click', iniciaJogo);
 
-function iniciaJogo (){
+function iniciaJogo() {
     atual = 0;
     historiaFinal = "";
     telaInicial.style.display = 'none';
     caixaPerguntas.classList.remove("mostrar");
-    caixaAlternativas.classList.remove("mostrar")
-    caixaResultados.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultado.classList.remove("mostrar");
     mostraPergunta();
 }
 
-function mostraPergunta(){
-    if (atual >= perguntas.length){
-        mostraResultados();
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
         return;
-  }
-     perguntaAtual = perguntas[atual];
-  caixaPerguntas.textContent = perguntaAtual.enunciado;
-  caixaAlternativas.textContent = "";
-  mostraAlternativas();
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
 
-function mostraAlternativas(){
-    for (const alternativa of perguntaAtual.alternativa){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa-texto;
-        botaoAlternativas.EddEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendchild(botaoAlternativas);
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = aleatorio (opcaoSelecionada.afirmacao);
-    historiaFinal += afirmacoes +"";
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+    historiaFinal += afirmacoes + " ";
     if (opcaoSelecionada.proxima !== undefined) {
         atual = opcaoSelecionada.proxima;
     } else {
@@ -58,21 +58,25 @@ function respostaSelecionada(opcaoSelecionada) {
     mostraPergunta();
 }
 
-function mostraResultado(){
-    caixaPerguntas.textContent = 'em 2049, ${nome}';
+function mostraResultado() {
+    caixaPerguntas.textContent = `Em 2049, ${nome}`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
-    caixaResultados.classList.add("mostrar");
-    botaoJogarNovamente.addEventListener("click", jogaNovamnete);
+    caixaResultado.classList.add("mostrar");
+    botaoJogarNovamente.addEventListener("click", jogaNovamente);
 }
 
-function jogaNovamnete (){
+function jogaNovamente() {
     atual = 0;
     historiaFinal = "";
-    caixaResultados.classList.remove("mostrar");
+    caixaResultado.classList.remove("mostrar");
     mostraPergunta();
 }
 
-function substituinome() {
-    pergunta.enunciado = pergunta.enunciado.replace(/vovê/g, nome;)
+function substituiNome() {
+    for (const pergunta of perguntas) {
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+    }
 }
+
+substituiNome();
